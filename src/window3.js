8590ae6d397a80864d2469effa846a828120d522
Rodrigo Weilg <contact@rodrigoweilg.com>
2020-05-15 15:19:59 +0200

    input text css

--- src/window3.js
+++ src/window3.js
@@ -6,9 +6,19 @@ function Window3() {
     <div className="canvas-container">
       <div className="title-bar">
         <h1 className="bigtitle">Messages</h1>
+        <div className="separator"></div>
       </div>
       <div className="content-area">
-        <h3>section 3</h3>
+        <h3></h3>
+        <div className="text-field-container">
+          <input
+            type="text"
+            name="nome"
+            placeholder="Type something here..  Hit enter to send"
+            className="text-field"
+          />
+          {/* <input type="send" value="Send" className="send-btn" /> */}
+        </div>
       </div>
     </div>
   );
